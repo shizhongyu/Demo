4db68031c4191fd745d4dc928e386b1203aa88a4
microli <microli@tencent.com>
2021-05-12 12:23:46 +0800

    事件分发。

---
+++ app/src/main/java/com/microli/demo/MainBean.java
@@ -0,0 +1,11 @@
+package com.microli.demo;
+
+public class MainBean {
+    public String name;
+    public Class className;
+
+    public MainBean(String name, Class className) {
+        this.name = name;
+        this.className = className;
+    }
+}
