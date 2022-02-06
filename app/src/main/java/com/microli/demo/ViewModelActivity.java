37e2d5ded36616d5ae6fa7de49e42e74db9c0b18
microli <microli@tencent.com>
2022-02-06 14:49:03 +0800

    viewmodel

---
+++ app/src/main/java/com/microli/demo/ViewModelActivity.java
@@ -0,0 +1,51 @@
+package com.microli.demo;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.List;
+
+/**
+ * Copyright (c) 2021 Tencent. All rights reserved.
+ * 类功能描述:
+ *
+ * @Author microli
+ * 2022/2/6
+ */
+public class ViewModelActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewmodel_ac);
+        MyViewModel model = new ViewModelProvider(this).get(MyViewModel.class);
+        model.getUsers().observe(this, users -> {
+            // update UI
+        });
+    }
+
+    public class MyViewModel extends ViewModel {
+        private MutableLiveData<List<User>> users;
+
+        public LiveData<List<User>> getUsers() {
+            if (users == null) {
+                users = new MutableLiveData<List<User>>();
+                loadUsers();
+            }
+            return users;
+        }
+
+        private void loadUsers() {
+            // Do an asynchronous operation to fetch users.
+        }
+    }
+
+    public class User {
+
+    }
+}
