06cba86da43836dab3cebec54620a74d3a7fa333
microli <microli@tencent.com>
2022-02-08 21:47:38 +0800

    SharedPreferences

--- app/src/main/java/com/microli/demo/MainActivity.java
+++ app/src/main/java/com/microli/demo/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initData() {
+        classes.add(new MainBean("SharedPreferences", SharedPreferencesActivity.class));
         classes.add(new MainBean("DataBinding", DataBindingActivity.class));
         classes.add(new MainBean("LiveData", LiveDataActivity.class));
         classes.add(new MainBean("ViewModel", ViewModelActivity.class));
---
+++ app/src/main/java/com/microli/demo/SharedPreferencesActivity.java
@@ -0,0 +1,43 @@
+package com.microli.demo;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Copyright (c) 2021 Tencent. All rights reserved.
+ * 类功能描述:
+ *
+ * @Author microli
+ * 2022/2/8
+ */
+public class SharedPreferencesActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sharedpreferences_ac);
+        putValue();
+    }
+
+    private void putValue() {
+        SharedPreferences sharedPreferences = getSharedPreferences("gityuan", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("blog", "www.gityuan.com");
+        editor.putInt("years", 3);
+        editor.commit();
+    }
+
+    private String getValue() {
+        SharedPreferences sharedPreferences = getSharedPreferences("gityuan", Context.MODE_PRIVATE);
+        return sharedPreferences.getString("blog", "null");
+    }
+
+    public void onClick(View view) {
+        ToastUtils.showShort(this, getValue());
+    }
+}
