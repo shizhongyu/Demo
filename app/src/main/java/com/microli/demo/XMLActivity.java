ce1a411672c9975b3fc493f59f6834e5089b6d9f
microli <microli@tencent.com>
2022-04-01 10:36:36 +0800

    xml

---
+++ app/src/main/java/com/microli/demo/XMLActivity.java
@@ -0,0 +1,21 @@
+package com.microli.demo;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Copyright (c) 2021 Tencent. All rights reserved.
+ * 类功能描述:
+ *
+ * @Author microli
+ * @date 2022/4/1
+ */
+public class XMLActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xml_ac);
+    }
+}
