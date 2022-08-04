a485f3e5487a0cb54fe9c2a4d6d6c8fcc6653925
microli <microli@tencent.com>
2022-08-04 10:44:27 +0800

    锁

---
+++ app/src/main/java/com/microli/demo/SynchrogazerActivity.java
@@ -0,0 +1,19 @@
+package com.microli.demo;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class SynchrogazerActivity extends BaseActivity{
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.synchronized_layout);
+    }
+
+    public class MainClass {
+
+    }
+}
